93d640a2854d47b0241a1670a688842ee1a25792
korynguyen <60016589+korynguyen@users.noreply.github.com>
2021-04-07 19:18:24 -0500

    Update WebForm1.aspx.cs

--- Zoo2/WebForm1.aspx.cs
+++ Zoo2/WebForm1.aspx.cs
@@ -11,6 +11,7 @@ namespace Zoo2
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        // Database connection string
         string connection = "Server=team4zoodb.mysql.database.azure.com; Port=3306; Database=zoo; Uid=Team4@team4zoodb; Pwd=4thTeamRocks; SslMode=Preferred;";
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -19,22 +20,24 @@ namespace Zoo2
 
         protected void Button1_Click(object sender, EventArgs e)
         {          
+            //Specify and connect to the DB
            using(MySqlConnection sqlConnection = new MySqlConnection(connection))
             {
                 sqlConnection.Open();
+                // Call the stored routine submitemployee (I custom created it inside mySQL) which will insert all the info from the webform. The parameter are in the comment below
                 MySqlCommand sqlCmd = new MySqlCommand("submitemployee", sqlConnection);//(@s_ID, @s_FName, @s_MInitial, @s_LName, @s_StartDate, @s_Addr, @s_Phone, @s_Sex, @s_DOB)
-                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
-                sqlCmd.Parameters.AddWithValue("s_ID", TextBox1.Text);
+                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure; // specify the command "submit employee" above, which is a Stored Procedure/Routine
+                sqlCmd.Parameters.AddWithValue("s_ID", TextBox1.Text); // Get values entered from the textboxes into the respective variable
                 sqlCmd.Parameters.AddWithValue("s_FName", TextBox4.Text);
-                sqlCmd.Parameters.AddWithValue("s_MInitial", TextBox5.Text);
+                sqlCmd.Parameters.AddWithValue("s_MInitial", TextBox5.Text); // the numbers are all over the place because I didn't design it carefully, don't mind it
                 sqlCmd.Parameters.AddWithValue("s_LName", TextBox6.Text);
                 sqlCmd.Parameters.AddWithValue("s_StartDate", TextBox2.Text);
                 sqlCmd.Parameters.AddWithValue("s_Addr", TextBox3.Text);
                 sqlCmd.Parameters.AddWithValue("s_Phone", TextBox7.Text);
                 sqlCmd.Parameters.AddWithValue("s_Sex", TextBox8.Text);
                 sqlCmd.Parameters.AddWithValue("s_DOB", TextBox9.Text);
-                sqlCmd.ExecuteNonQuery();
-                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Form Submitted" + "');", true); 
+                sqlCmd.ExecuteNonQuery(); // execute query
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Form Submitted" + "');", true); // pop up message when it is done submitting
                 sqlConnection.Close();
             }
         }
