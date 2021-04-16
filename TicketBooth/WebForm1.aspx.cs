f5a76dbeee110916196ed3d1d3ee6d86f6dbece1
ShadmunTalukderShahed <shadmuntalukder@gmail.com>
2021-04-15 21:42:52 -0500

    Added Ticket Booth

---
+++ TicketBooth/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace TicketBooth
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connection = "Server=team4zoodb.mysql.database.azure.com; Port=3306; Database=zoo; Uid=Team4@team4zoodb; Pwd=4thTeamRocks; SslMode=Preferred;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            //Specify and connect to the DB
+            using (MySqlConnection sqlConnection = new MySqlConnection(connection))
+            {
+                string sql = "INSERT INTO 'ticket booth' (BoothID) VALUES (" + BoothID.Text + ");";
+
+                sqlConnection.Open();
+                // Call the stored routine BoothID (I custom created it inside mySQL) which will insert all the info from the webform. The parameter are in the comment below
+                MySqlCommand sqlCmd = new MySqlCommand(sql, sqlConnection);//(@s_ID, @s_FName, @s_MInitial, @s_LName, @s_StartDate, @s_Addr, @s_Phone, @s_Sex, @s_DOB)
+
+
+                sqlCmd.ExecuteNonQuery(); // execute query
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "Form Submitted" + "');", true); // pop up message when it is done submitting
+                sqlConnection.Close();
+            }
+        }
+    }
+}
